14c2736bb47388a21a5e2a1fe2f7fa7c56ad561b
Ravi <132803604+rkyravi@users.noreply.github.com>
2025-08-12 09:41:31 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This is my first Python program
+
+# Printing Hello World
+print("Hello World!")
+
+# Storing name and age in variables
+name = "Yadav"  # Apna naam likho
+age = 20         # Apni age likho
+
+# Printing name and age
+print("My name is", name, "and I am", age, "years old.")
